59e12fa64dec7dda18d2edc81e85e89bdae77dbb
CamillaVoss <thecamillavoss@gmail.com>
2024-03-29 13:25:24 +0100

    Fetch recipe data

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
                 class="z-10 absolute top-2 right-2 py-1.5 px-4 bg-orange-400 rounded-md shadow-md text-white">
                 {{ recipe.portions }} portions
         </div>
-            <a :href="'/recipes/' + recipe.id">
+            <a :href="'/recipes/' + recipe.id + '-' + recipe.slug">
                 <div class="relative h-52 w-full overflow-hidden">
                     <img class="absolute inset-0 w-full h-full object-cover"
                         :src="recipe.image || 'https://images.unsplash.com/photo-1543352632-5a4b24e4d2a6?q=80&w=3725&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'"
@@ -33,10 +33,6 @@
                         <div>
                             <p class="font-medium text-orange-400">{{ Math.round(recipe.protein) || 0 }}</p>
                             <p class="text-xs text-orange-950/40">protein</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">{{ Math.round(recipe.fat) || 0 }}</p>
-                            <p class="text-xs text-orange-950/40">fat</p>
                         </div>
                         <div>
                             <p class="font-medium text-orange-400">{{ Math.round(recipe.fibres) || 0 }}</p>
--- pages/recipes/[id]-[slug].vue
+++ pages/recipes/[id]-[slug].vue
@@ -1,9 +1,8 @@
 <template>
     <div class="lg:flex lg:items-start lg:justify-between">
         <div class="min-w-0 flex-1">
-            <h2 class="text-2xl font-bold leading-7 text-gray-700 sm:truncate sm:text-3xl sm:tracking-tight">Recipe name
-            </h2>
-            <div class="mt-3 flex sm:flex-wrap sm:space-x-2">
+            <h2 class="text-2xl font-bold leading-7 text-gray-700 sm:truncate sm:text-3xl sm:tracking-tight"> {{ data.recipe.name }} </h2>
+            <div class="hidden mt-3 sm:flex-wrap sm:space-x-2">
                 <button
                     class="inline-flex items-center rounded-md bg-orange-50 px-2 py-1 text-xs font-medium text-orange-500 ring-1 ring-inset ring-orange-500/10 hover:opacity-50">Kylling</button>
                 <button
@@ -25,7 +24,7 @@
                 <Icon icon="lets-icons:folder-dublicate-light" class="text-gray-700 h-6 w-6" />
                 Duplicate
             </button>
-            <a href="/edit"
+            <a :href="'/edit/' + route.params.id"
                 class="inline-flex items-center gap-2 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                 <Icon icon="circum:edit" class="text-gray-700 h-6 w-6" />
                 Edit
@@ -35,24 +34,28 @@
     <div class="mt-6">
         <div class="relative h-40 md:h-80 w-full overflow-hidden rounded-t-xl">
             <img class="absolute inset-0 w-full h-full object-cover"
-                src="https://images.unsplash.com/photo-1543352632-5a4b24e4d2a6?q=80&w=3725&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                :src="data.recipe.image || 'https://images.unsplash.com/photo-1543352632-5a4b24e4d2a6?q=80&w=3725&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'"
                 alt="fooood">
                 <div class="absolute bottom-0 left-1/2 -translate-x-1/2 bg-orange-50 flex gap-8 justify-center text-center px-4 @[300px]/grid:px-12 py-2 rounded-t-md">
                     <div>
-                        <p class="font-medium text-orange-400">600</p>
+                        <p class="font-medium text-orange-400">{{ Math.round(data.recipe.energy) || 0 }}</p>
                         <p class="text-xs text-orange-950/50">kcal</p>
                     </div>
                     <div>
-                        <p class="font-medium text-orange-400">60</p>
+                        <p class="font-medium text-orange-400">{{ Math.round(data.recipe.protein) || 0 }}</p>
                         <p class="text-xs text-orange-950/50">protein</p>
                     </div>
                     <div>
-                        <p class="font-medium text-orange-400">10</p>
+                        <p class="font-medium text-orange-400">{{ Math.round(data.recipe.fat) || 0 }}</p>
                         <p class="text-xs text-orange-950/50">fat</p>
                     </div>
                     <div>
-                        <p class="font-medium text-orange-400">4</p>
-                        <p class="text-xs text-orange-950/50">fibers</p>
+                        <p class="font-medium text-orange-400">{{ Math.round(data.recipe.carbs) || 0 }}</p>
+                        <p class="text-xs text-orange-950/50">carbs</p>
+                    </div>
+                    <div>
+                        <p class="font-medium text-orange-400">{{ Math.round(data.recipe.fibres) || 0 }}</p>
+                        <p class="text-xs text-orange-950/50">fibres</p>
                     </div>
                 </div>
         </div>
@@ -132,10 +135,14 @@
 }
 </style>
 
-<script setup>
+<script setup lang="ts">
     import { Icon } from '@iconify/vue';
     import { Collapse } from 'vue-collapsed';
     import DeleteModal from "../components/modals/DeleteModal.vue";
+
+    const route = useRoute()
+
+    const { data, pending, error, refresh } = await useRecipe(route.params.id);
 
     const deleteModalOpen = ref(false);
     const showIngredientsList = ref(true);
