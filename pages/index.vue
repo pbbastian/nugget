256ca4f499089dd82eb5f2ee5255f426528852f1
CamillaVoss <thecamillavoss@gmail.com>
2024-03-29 11:44:40 +0100

    Reformat directories

--- pages/index.vue
+++ pages/index.vue
@@ -12,171 +12,35 @@
         </div>
     </div>
     <div class="grid mt-3 sm:mt-6 grid-cols-1 @md:grid-cols-2 @3xl:grid-cols-3 @6xl:grid-cols-4 gap-6 @3xl:gap-10">
-        <article class="relative rounded-md bg-white border hover:shadow-md transition-all duration-500">
-            <button
-                class="z-10 absolute top-2 right-2 p-1.5 bg-orange-400 rounded-md shadow-md hover:bg-orange-300 transition-color duration-500">
-                <Icon icon="fluent:clipboard-bullet-list-rtl-20-regular" class="text-white h-6 w-6" />
-            </button>
-            <a href="/recipe">
+        <article v-for="recipe in data.recipes" class="relative rounded-md bg-white border hover:shadow-md transition-all duration-500">
+            <div
+                class="z-10 absolute top-2 right-2 py-1.5 px-4 bg-orange-400 rounded-md shadow-md text-white">
+                {{ recipe.portions }} portions
+        </div>
+            <a :href="'/recipes/' + recipe.id">
                 <div class="relative h-52 w-full overflow-hidden">
                     <img class="absolute inset-0 w-full h-full object-cover"
-                        src="https://images.unsplash.com/photo-1543352632-5a4b24e4d2a6?q=80&w=3725&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        :src="recipe.image || 'https://images.unsplash.com/photo-1543352632-5a4b24e4d2a6?q=80&w=3725&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'"
                         alt="fooood">
                 </div>
                 <div class="py-5 px-6 @container/grid">
-                    <h3 class="text-lg font-semibold text-orange-950 text-center">New food</h3>
+                    <h3 class="text-lg font-semibold text-orange-950 text-center">{{ recipe.name }}</h3>
                     <div class="mt-3 flex gap-x-8 gap-y-4 flex-wrap @[300px]/grid:flex-nowrap justify-center text-center">
                         <div>
-                            <p class="font-medium text-orange-400">600</p>
+                            <p class="font-medium text-orange-400">{{ Math.round(recipe.energy) || 0 }}</p>
                             <p class="text-xs text-orange-950/40">kcal</p>
                         </div>
                         <div>
-                            <p class="font-medium text-orange-400">60</p>
+                            <p class="font-medium text-orange-400">{{ Math.round(recipe.protein) || 0 }}</p>
                             <p class="text-xs text-orange-950/40">protein</p>
                         </div>
                         <div>
-                            <p class="font-medium text-orange-400">10</p>
+                            <p class="font-medium text-orange-400">{{ Math.round(recipe.fat) || 0 }}</p>
                             <p class="text-xs text-orange-950/40">fat</p>
                         </div>
                         <div>
-                            <p class="font-medium text-orange-400">4</p>
-                            <p class="text-xs text-orange-950/40">fibers</p>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </article>
-        <article class="relative rounded-md bg-white border hover:shadow-md transition-all duration-500">
-            <button
-                class="z-10 absolute top-2 right-2 p-1.5 bg-orange-400 rounded-md shadow-md hover:bg-orange-300 transition-color duration-500">
-                <Icon icon="fluent:clipboard-bullet-list-rtl-20-regular" class="text-white h-6 w-6" />
-            </button>
-            <a href="/recipe">
-                <div class="relative h-52 w-full overflow-hidden">
-                    <img class="absolute inset-0 w-full h-full object-cover"
-                        src="https://images.unsplash.com/photo-1543352632-5a4b24e4d2a6?q=80&w=3725&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        alt="fooood">
-                </div>
-                <div class="py-5 px-6 @container/grid">
-                    <h3 class="text-lg font-semibold text-orange-950 text-center">New food</h3>
-                    <div class="mt-3 flex gap-x-8 gap-y-4 flex-wrap @[300px]/grid:flex-nowrap justify-center text-center">
-                        <div>
-                            <p class="font-medium text-orange-400">600</p>
-                            <p class="text-xs text-orange-950/40">kcal</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">60</p>
-                            <p class="text-xs text-orange-950/40">protein</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">10</p>
-                            <p class="text-xs text-orange-950/40">fat</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">4</p>
-                            <p class="text-xs text-orange-950/40">fibers</p>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </article>
-        <article class="relative rounded-md bg-white border hover:shadow-md transition-all duration-500">
-            <button
-                class="z-10 absolute top-2 right-2 p-1.5 bg-orange-400 rounded-md shadow-md hover:bg-orange-300 transition-color duration-500">
-                <Icon icon="fluent:clipboard-bullet-list-rtl-20-regular" class="text-white h-6 w-6" />
-            </button>
-            <a href="/recipe">
-                <div class="relative h-52 w-full overflow-hidden">
-                    <img class="absolute inset-0 w-full h-full object-cover"
-                        src="https://images.unsplash.com/photo-1543352632-5a4b24e4d2a6?q=80&w=3725&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        alt="fooood">
-                </div>
-                <div class="py-5 px-6 @container/grid">
-                    <h3 class="text-lg font-semibold text-orange-950 text-center">New food</h3>
-                    <div class="mt-3 flex gap-x-8 gap-y-4 flex-wrap @[300px]/grid:flex-nowrap justify-center text-center">
-                        <div>
-                            <p class="font-medium text-orange-400">600</p>
-                            <p class="text-xs text-orange-950/40">kcal</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">60</p>
-                            <p class="text-xs text-orange-950/40">protein</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">10</p>
-                            <p class="text-xs text-orange-950/40">fat</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">4</p>
-                            <p class="text-xs text-orange-950/40">fibers</p>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </article>
-        <article class="relative rounded-md bg-white border hover:shadow-md transition-all duration-500">
-            <button
-                class="z-10 absolute top-2 right-2 p-1.5 bg-orange-400 rounded-md shadow-md hover:bg-orange-300 transition-color duration-500">
-                <Icon icon="fluent:clipboard-bullet-list-rtl-20-regular" class="text-white h-6 w-6" />
-            </button>
-            <a href="/recipe">
-                <div class="relative h-52 w-full overflow-hidden">
-                    <img class="absolute inset-0 w-full h-full object-cover"
-                        src="https://images.unsplash.com/photo-1543352632-5a4b24e4d2a6?q=80&w=3725&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        alt="fooood">
-                </div>
-                <div class="py-5 px-6 @container/grid">
-                    <h3 class="text-lg font-semibold text-orange-950 text-center">New food</h3>
-                    <div class="mt-3 flex gap-x-8 gap-y-4 flex-wrap @[300px]/grid:flex-nowrap justify-center text-center">
-                        <div>
-                            <p class="font-medium text-orange-400">600</p>
-                            <p class="text-xs text-orange-950/40">kcal</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">60</p>
-                            <p class="text-xs text-orange-950/40">protein</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">10</p>
-                            <p class="text-xs text-orange-950/40">fat</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">4</p>
-                            <p class="text-xs text-orange-950/40">fibers</p>
-                        </div>
-                    </div>
-                </div>
-            </a>
-        </article>
-        <article class="relative rounded-md bg-white border hover:shadow-md transition-all duration-500">
-            <button
-                class="z-10 absolute top-2 right-2 p-1.5 bg-orange-400 rounded-md shadow-md hover:bg-orange-300 transition-color duration-500">
-                <Icon icon="fluent:clipboard-bullet-list-rtl-20-regular" class="text-white h-6 w-6" />
-            </button>
-            <a href="/recipe">
-                <div class="relative h-52 w-full overflow-hidden">
-                    <img class="absolute inset-0 w-full h-full object-cover"
-                        src="https://images.unsplash.com/photo-1543352632-5a4b24e4d2a6?q=80&w=3725&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        alt="fooood">
-                </div>
-                <div class="py-5 px-6 @container/grid">
-                    <h3 class="text-lg font-semibold text-orange-950 text-center">New food</h3>
-                    <div class="mt-3 flex gap-x-8 gap-y-4 flex-wrap @[300px]/grid:flex-nowrap justify-center text-center">
-                        <div>
-                            <p class="font-medium text-orange-400">600</p>
-                            <p class="text-xs text-orange-950/40">kcal</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">60</p>
-                            <p class="text-xs text-orange-950/40">protein</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">10</p>
-                            <p class="text-xs text-orange-950/40">fat</p>
-                        </div>
-                        <div>
-                            <p class="font-medium text-orange-400">4</p>
-                            <p class="text-xs text-orange-950/40">fibers</p>
+                            <p class="font-medium text-orange-400">{{ Math.round(recipe.fibres) || 0 }}</p>
+                            <p class="text-xs text-orange-950/40">fibres</p>
                         </div>
                     </div>
                 </div>
@@ -185,6 +49,8 @@
     </div>
 </template>
   
-<script setup>
+<script setup lang="ts">
 import { Icon } from '@iconify/vue';
+
+const { data, pending, error, refresh } = await useRecipes();
 </script>
