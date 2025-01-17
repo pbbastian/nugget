ca2efea2d6a662c7cea0d0b9ed5ece3c9ce94353
CamillaVoss <thecamillavoss@gmail.com>
2025-01-17 15:00:07 +0100

    Add carbs to modal

--- components/modals/IngredientModal.vue
+++ components/modals/IngredientModal.vue
@@ -22,9 +22,8 @@
                                             Information
                                         </h2>
 
-                                        <div
-                                            class="border-t border-gray-900/10 pt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-8">
-                                            <div class="sm:col-span-4">
+                                        <div class="border-t border-gray-900/10 pt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-10">
+                                            <div class="sm:col-span-5">
                                                 <label for="name"
                                                     class="block text-sm font-medium leading-6 text-gray-900">Name</label>
                                                 <div class="mt-2">
@@ -33,7 +32,7 @@
                                                 </div>
                                             </div>
 
-                                            <div class="sm:col-span-4">
+                                            <div class="sm:col-span-5">
                                                 <label for="vendor"
                                                     class="block text-sm font-medium leading-6 text-gray-900">Vendor</label>
                                                 <div class="mt-2">
@@ -42,7 +41,7 @@
                                                 </div>
                                             </div>
 
-                                            <div class="sm:col-span-4">
+                                            <div class="sm:col-span-5">
                                                 <label for="density"
                                                     class="block text-sm font-medium leading-6 text-gray-900">Density (g/ml) (optional)</label>
                                                 <div class="mt-2">
@@ -51,7 +50,7 @@
                                                 </div>
                                             </div>
 
-                                            <div class="sm:col-span-4">
+                                            <div class="sm:col-span-5">
                                                 <label for="weight"
                                                     class="block text-sm font-medium leading-6 text-gray-900">Weight (g/pc) (optional)</label>
                                                 <div class="mt-2">
@@ -79,6 +78,24 @@
                                             </div>
 
                                             <div class="sm:col-span-2">
+                                                <label for="fibers"
+                                                       class="block text-sm font-medium leading-6 text-gray-900">Fibers</label>
+                                                <div class="mt-2">
+                                                    <input type="number" name="fibers" id="fibers" placeholder="4" v-model="data.fibres"
+                                                           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6" />
+                                                </div>
+                                            </div>
+
+                                            <div class="sm:col-span-2">
+                                                <label for="fat"
+                                                       class="block text-sm font-medium leading-6 text-gray-900">Carbs</label>
+                                                <div class="mt-2">
+                                                    <input type="number" name="fat" id="fat" placeholder="15" v-model="data.carbs"
+                                                           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6" />
+                                                </div>
+                                            </div>
+
+                                            <div class="sm:col-span-2">
                                                 <label for="fat"
                                                     class="block text-sm font-medium leading-6 text-gray-900">Fat</label>
                                                 <div class="mt-2">
@@ -87,14 +104,6 @@
                                                 </div>
                                             </div>
 
-                                            <div class="sm:col-span-2">
-                                                <label for="fibers"
-                                                    class="block text-sm font-medium leading-6 text-gray-900">Fibers</label>
-                                                <div class="mt-2">
-                                                    <input type="number" name="fibers" id="fibers" placeholder="4" v-model="data.fibres"
-                                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-400 sm:text-sm sm:leading-6" />
-                                                </div>
-                                            </div>
                                         </div>
                                     </div>
                                     <div class="mt-6 flex items-center justify-end gap-x-6">
