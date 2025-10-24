266ae1606370eee4782e930217ccfdc154764231
CamillaVoss <thecamillavoss@gmail.com>
2025-10-24 11:01:07 +0200

    Fix null bug in ingredient modal

--- server/api/ingredients/[id].put.ts
+++ server/api/ingredients/[id].put.ts
@@ -27,16 +27,16 @@ export default defineEventHandler(async (event) => {
             UPDATE recipe_ingredients ri
             SET weight =
                 CASE
-                    WHEN ri.unit = 'pk' OR ri.unit = 'stk' THEN ri.amount * i.weight
+                    WHEN ri.unit = 'pk' OR ri.unit = 'stk' THEN ri.amount * COALESCE(i.weight, 0)
                     WHEN ri.unit = 'g' THEN ri.amount
                     WHEN ri.unit = 'kg' THEN ri.amount * 1000.0
-                    WHEN ri.unit = 'ml' THEN i.density * ri.amount
-                    WHEN ri.unit = 'cl' THEN i.density * ri.amount * 10.0
-                    WHEN ri.unit = 'dl' THEN i.density * ri.amount * 100.0
-                    WHEN ri.unit = 'l' THEN i.density * ri.amount * 1000.0
-                    WHEN ri.unit = 'knsp' THEN i.density * ri.amount * 0.25
-                    WHEN ri.unit = 'tsk' THEN i.density * ri.amount * 5.0
-                    WHEN ri.unit = 'spsk' THEN i.density * ri.amount * 15.0
+                    WHEN ri.unit = 'ml' THEN COALESCE(i.density, 0) * ri.amount
+                    WHEN ri.unit = 'cl' THEN COALESCE(i.density, 0) * ri.amount * 10.0
+                    WHEN ri.unit = 'dl' THEN COALESCE(i.density, 0) * ri.amount * 100.0
+                    WHEN ri.unit = 'l' THEN COALESCE(i.density, 0) * ri.amount * 1000.0
+                    WHEN ri.unit = 'knsp' THEN COALESCE(i.density, 0) * ri.amount * 0.25
+                    WHEN ri.unit = 'tsk' THEN COALESCE(i.density, 0) * ri.amount * 5.0
+                    WHEN ri.unit = 'spsk' THEN COALESCE(i.density, 0) * ri.amount * 15.0
                 END
             FROM ingredients i WHERE i.id = ${params.id}
         `))
@@ -44,15 +44,15 @@ export default defineEventHandler(async (event) => {
     promises.push(client.query(sql`
             UPDATE recipes as r
             SET (energy, fat, carbs, fibres, protein) = (
-            	SELECT
-            		sum(ri.weight * i.energy) / (r.portions * 100.0),
-            		sum(ri.weight * i.fat) / (r.portions * 100.0),
-            		sum(ri.weight * i.carbs) / (r.portions * 100.0),
-            		sum(ri.weight * i.fibres) / (r.portions * 100.0),
-            		sum(ri.weight * i.protein) / (r.portions * 100.0)
-            	FROM recipe_ingredients ri
-            	JOIN ingredients i ON i.id = ri.ingredient
-            	WHERE ri.recipe = r.id
+                SELECT
+                    sum(ri.weight * i.energy) / (r.portions * 100.0),
+                    sum(ri.weight * i.fat) / (r.portions * 100.0),
+                    sum(ri.weight * i.carbs) / (r.portions * 100.0),
+                    sum(ri.weight * i.fibres) / (r.portions * 100.0),
+                    sum(ri.weight * i.protein) / (r.portions * 100.0)
+                FROM recipe_ingredients ri
+                JOIN ingredients i ON i.id = ri.ingredient
+                WHERE ri.recipe = r.id
             )
             WHERE r.id = (SELECT DISTINCT ri.recipe FROM recipe_ingredients ri WHERE ri.ingredient = ${params.id})
         `))
@@ -60,7 +60,7 @@ export default defineEventHandler(async (event) => {
     const result = await resultPromise
     await Promise.all(promises)
 
-    if (result.rowCount == 0) {
+    if (result.rowCount === 0) {
       setResponseStatus(event, 404)
       await client.query('ROLLBACK')
     }
