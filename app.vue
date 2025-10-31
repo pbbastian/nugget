692acab7e44f3342b6d9745c6c48a4a60d2670ac
CamillaVoss <thecamillavoss@gmail.com>
2025-10-31 10:45:54 +0100

    Navigation design

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { Icon } from '@iconify/vue'
-import ToastContainer from '~/components/ToastContainer.vue'
+
+const route = useRoute()
+const isRecipesActive = computed(() => route.path === '/' || route.path.startsWith('/recipes') || route.path.startsWith('/edit'))
+const isIngredientsActive = computed(() => route.path.startsWith('/ingredients'))
 </script>
 
 <template>
@@ -15,11 +18,17 @@ import ToastContainer from '~/components/ToastContainer.vue'
             <li>
               <a
                 href="/"
-                class="transition-color group flex items-center gap-2 rounded-md p-2 duration-300 hover:bg-orange-50 hover:text-orange-400 lg:gap-3"
+                class="transition-color group flex items-center gap-2 rounded-md p-2 duration-300 lg:gap-3"
+                :class="[
+                  isRecipesActive && 'bg-gray-50 text-orange-400',
+                ]"
               >
                 <Icon
                   icon="ph:bowl-food-light"
-                  class="transition-color size-6 text-gray-400 duration-300 group-hover:text-orange-400 max-sm:hidden"
+                  class="transition-color size-6 duration-300 group-hover:text-orange-400 max-sm:hidden"
+                  :class="[
+                    isRecipesActive ? 'text-orange-400' : 'text-gray-400',
+                  ]"
                 />
                 Recipes
               </a>
@@ -27,25 +36,23 @@ import ToastContainer from '~/components/ToastContainer.vue'
             <li>
               <a
                 href="/ingredients"
-                class="transition-color group flex items-center gap-2 rounded-md p-2 duration-300 hover:bg-orange-50 hover:text-orange-400 lg:gap-3"
+                class="transition-color group flex items-center gap-2 rounded-md p-2 duration-300 lg:gap-3 hover:bg-orange-50 hover:text-orange-400"
+                :class="[
+                  isIngredientsActive && 'bg-gray-50 text-orange-400',
+                ]"
               >
                 <Icon
                   icon="fluent:food-carrot-20-regular"
-                  class="transition-color size-6 text-gray-400 duration-300 group-hover:text-orange-400 max-sm:hidden"
+                  class="transition-color size-6 duration-300 group-hover:text-orange-400 max-sm:hidden"
+                  :class="[
+                    isIngredientsActive ? 'text-orange-400' : 'text-gray-400',
+                  ]"
                 />
                 Ingredients
               </a>
             </li>
           </ul>
         </nav>
-        <!-- <a href="#"
-          class="text-gray-900 font-semibold text-sm flex items-center gap-4 -mx-6 px-6 py-4 hover:bg-orange-50">
-          <img class="h-8 w-8 rounded-full"
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
-          alt="">
-        <span class="sr-only">Your profile</span>
-        <span aria-hidden="true">Peter Bay Bastian</span>
-      </a> -->
       </div>
     </aside>
     <main class="relative max-lg:pt-16 lg:min-h-screen lg:pl-72">
